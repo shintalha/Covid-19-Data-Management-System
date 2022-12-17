fb5d86fc295b99d9f674a7135c86fb8040e008b1
shintalha <sahintalha0@outlook.com>
2022-12-17 14:35:43 +0300

    additional fixes

--- app.py
+++ app.py
@@ -12,6 +12,7 @@ app.add_url_rule("/", view_func=login_page, methods=["GET", "POST", "POST2"])
 
 app.add_url_rule("/locations", view_func=locations_page)
 app.add_url_rule("/locations/<loc_name>", view_func=location_page)
+
 app.add_url_rule("/patients", view_func=patients_page, methods=["GET", "POST"])
 app.add_url_rule("/patients/add", view_func=add_patients_data, methods=["GET", "POST"])
 app.add_url_rule("/patients/update", view_func=update_patients_data, methods=["GET", "POST"])
