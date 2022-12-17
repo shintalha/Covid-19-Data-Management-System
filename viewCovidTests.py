3bc0ebc730d3129e8d3b6fe49332f3fc033f1c60
ituitis20-arabaci19 <arabaci19@itu.edu.tr>
2022-12-18 01:51:06 +0300

    remove end date filter and location input

--- viewCovidTests.py
+++ viewCovidTests.py
@@ -1,6 +1,4 @@
-from flask import render_template, flash, request
-from werkzeug.utils import redirect
-from datetime import datetime,date
+from flask import render_template, request
 import numpy as np
 
 from model.covid_tests import *
@@ -9,35 +7,31 @@ def tests_page(id = -1):
     page_id = int(request.args.get('page')) if request.args.get('page') is not None else 1
     loc_name = request.args.get('loc_name') if request.args.get('loc_name') is not None else "?"
     date = request.args.get('date') if request.args.get('date') is not None else "?"
-    end_date = request.args.get('end_date') if request.args.get('end_date') is not None else "?"
+    table_size = 0
+
     covid_tests = CovidTests()
     if id != -1:
         covid_tests.delete(int(id))
-        
-    #page_id = 1
-    #loc_name = "Antigua_and_Barbuda"
-    #date = "2020-03-13"
-    #end_date = "?"
         
     loc_name = loc_name.replace("_"," ")
     
     loc_names = covid_tests.get_location_names()
     offset = (page_id-1)*50
     paginationValues = (page_id-1,page_id,page_id+1) if (page_id)>1 else (1,2,3)
-    covid_data = np.array(covid_tests.read(50,offset,loc_name,date,end_date))
-    while(covid_data.size == 0):
-        if offset == 0:
-            covid_data = np.zeros([1, len(covid_tests.columns)], dtype='str')
-            break
-        offset = 0
-        covid_data = np.array(covid_tests.read(50,offset,loc_name,date,end_date))
+    
+
+    try:
+        covid_data = np.array(covid_tests.read_filter(50,offset,loc_name,date))[:,0:9]
+        table_size = covid_data.size
+    except IndexError:
+        covid_data = np.array([[]])
+        table_size = 0
             
     start_dates = covid_tests.get_dates(loc_name)
     headers = covid_tests.columns
 
-    del covid_tests
-    return render_template("tests/tests.html", table_headers=headers, table_rows = covid_data[:,0:9], \
-        paginationValues=paginationValues, locations = loc_names, dates = start_dates) 
+    return render_template("tests/tests.html", table_headers=headers, table_rows = covid_data, \
+        paginationValues=paginationValues, locations = loc_names, dates = start_dates, data_available=table_size) 
 
 def add_tests_page():
     covid_test = CovidTests()
@@ -64,8 +58,7 @@ def update_tests_page():
     covid_test = CovidTests()
     row = np.array(covid_test.read_by_id(row_id))
     message = "empty"
-    if request.method == "POST":   
-        location_id = request.form["location_id"] if request.form["location_id"] !="" else row[1]
+    if request.method == "POST":
         total_tests = request.form["total_tests"] if request.form["total_tests"] !="" else row[2]
         new_tests = request.form["new_tests"] if request.form["new_tests"] !="" else row[3]
         total_tests_per_thousand = request.form["total_tests_per_thousand"] if request.form["total_tests_per_thousand"] !="" else row[4]
@@ -73,7 +66,7 @@ def update_tests_page():
         new_tests_smoothed = request.form["new_tests_smoothed"] if request.form["new_tests_smoothed"] !="" else row[6]
         positive_rate = request.form["positive_rate"] if request.form["positive_rate"] !="" else row[7]
         date_time = request.form["date_time"] if request.form["date_time"] !="" else row[8]
-        result = covid_test.update(row_id, location_id, total_tests, new_tests, total_tests_per_thousand, new_tests_per_thousand, new_tests_smoothed, positive_rate, date_time)
+        result = covid_test.update(row_id, row[1], total_tests, new_tests, total_tests_per_thousand, new_tests_per_thousand, new_tests_smoothed, positive_rate, date_time)
         if result:
             message = "success"  
         else:
