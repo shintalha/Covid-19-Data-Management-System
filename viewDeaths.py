8bfb3d3c5591ae0bb1b0e98ccaff35de46514c2c
zehraasan <asan19@itu.edu.tr>
2022-12-18 21:53:13 +0300

    add/update functions updated

--- viewDeaths.py
+++ viewDeaths.py
@@ -38,25 +38,25 @@ def deaths_page(id = -1):
             
     start_dates = deaths.get_dates(loc_name)
     headers = ["Location Id", "Total Deaths", "New Deaths", "New Deaths Smoothed",
-                "Total Deaths Per Million", "New Deaths Per Million", "New Deaths Smoothed Per Million","Date "]
+                "Total Deaths Per Million", "New Deaths Per Million", "New Deaths Smoothed Per Million", "Date"]
 
     return render_template("deaths/deaths.html", table_headers=headers, table_rows = covid_data, \
         paginationValues=paginationValues, locations = loc_names, dates = start_dates, data_available=table_size, is_admin=is_admin) 
 
 def add_deaths_page():
-    covid_test = Deaths()
+    deaths = Deaths()
     message = "empty"
 
     if request.method == "POST":   
         location_id = request.form["location_id"]
         total_deaths = request.form["total_deaths"]
-        new_deaths = request.form["new_deaths"] 
+        new_deaths_smoothed = request.form["new_deaths_smoothed"] 
         total_deaths_per_million = request.form["total_deaths_per_million"] if request.form["total_deaths_per_million"] !="" else None
-        new_deaths_per_million= request.form["new_deaths_per_million"] if request.form["new_deaths_per_million"] !="" else None
-        new_deaths_smoothed = request.form["new_deaths_smoothed"] if request.form["new_deaths_smoothed"] !="" else None
+        new_deaths_per_million = request.form["new_deaths_per_million"] if request.form["new_deaths_per_million"] !="" else None
         new_deaths_smoothed_per_million = request.form["new_deaths_smoothed_per_million"] if request.form["new_deaths_smoothed_per_million"] !="" else None
+        new_deaths = request.form["new_deaths"] if request.form["new_deaths"] !="" else None
         date_time = request.form["date_time"] if request.form["date_time"] !="" else None
-        result = covid_test.insert_row(location_id, total_deaths, new_deaths, total_deaths_per_million, new_deaths_per_million, new_deaths_smoothed, new_deaths_smoothed_per_million, date_time)
+        result = deaths.insert_row(location_id, total_deaths, new_deaths_smoothed, total_deaths_per_million, new_deaths_per_million, new_deaths_smoothed_per_million, new_deaths, date_time)
         if result:
             message = "success"  
         else:
@@ -64,22 +64,21 @@ def add_deaths_page():
     return render_template("deaths/add-deaths.html", message=message) 
 
 def update_deaths_page():
-    row_id = request.args.get('id')
-    
-    row_id = int(row_id)
 
+    row_id = request.args.get('id')
+    row_id = int(row_id)
     deaths = Deaths()
-    row = np.array(deaths.readFid(row_id))
+    row = np.array(deaths.read_with_id(row_id))
     message = "empty"
     if request.method == "POST":
         total_deaths = request.form["total_deaths"] if request.form["total_deaths"] !="" else row[2]
-        new_deaths = request.form["new_deaths"] if request.form["new_deaths"] !="" else row[3]
+        new_deaths_smoothed = request.form["new_deaths_smoothed"] if request.form["new_deaths_smoothed"] !="" else row[3]
         total_deaths_per_million = request.form["total_deaths_per_million"] if request.form["total_deaths_per_million"] !="" else row[4]
         new_deaths_per_million = request.form["new_deaths_per_million"] if request.form["new_deaths_per_million"] !="" else row[5]
-        new_deaths_smoothed = request.form["new_deaths_smoothed"] if request.form["new_deaths_smoothed"] !="" else row[6]
-        new_deaths_smoothed_per_million = request.form["new_deaths_smoothed_per_million"] if request.form["new_deaths_smoothed_per_million"] !="" else row[7]
+        new_deaths_smoothed_per_million = request.form["new_deaths_smoothed_per_million"] if request.form["new_deaths_smoothed_per_million"] !="" else row[6]
+        new_deaths = request.form["new_deaths"] if request.form["new_deaths"] !="" else row[7]
         date_time = request.form["date_time"] if request.form["date_time"] !="" else row[8]
-        result = deaths.update_row(row_id, row[1], total_deaths, new_deaths, total_deaths_per_million, new_deaths_per_million, new_deaths_smoothed, new_deaths_smoothed_per_million, date_time)
+        result = deaths.update_row(row_id, row[1], total_deaths, new_deaths_smoothed, total_deaths_per_million, new_deaths_per_million, new_deaths_smoothed_per_million, new_deaths, date_time)
         if result:
             message = "success"  
         else:
